6c93aa5c9dd0f6ecd187dd6ff9ec3d0223b250ef
Ryan Walters <RyanWaltersDev@gmail.com>
2021-08-17 21:26:58 -0400

    +More file reading projects

--- pi_birthday.py
+++ pi_birthday.py
@@ -1,15 +1,4 @@
-# Print pi digits as a string
-filename = 'text_files/pi_digits.txt'
-
-with open(filename) as file_object:
-    lines = file_object.readlines()
-
-pi_string = ''
-for line in lines:
-    pi_string += line.strip()
-
-print(pi_string)
-print(len(pi_string))
+#!/usr/bin/env python3
 
 # One million digits of pi
 filename = '../../CC2e resources/chapter_10/pi_million_digits.txt'
@@ -21,14 +10,12 @@ pi_string = ''
 for line in lines:
     pi_string += line.strip()
 
-print(f"{pi_string[:52]}...")
-print(len(pi_string))
-
 # Check for birthday in first million digits of pi
 
 birthday = input("Enter your birthday, in the form of mmddyy: ")
 if birthday in pi_string:
     print("Your birthday appears in the first million digits of pi!")
+    print(f"Position in pi: {pi_string.index(birthday)}")
 else:
     print("Your birthday does not appear in the first million digits of pi.")
 
