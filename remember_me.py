a884e404ca84bc4c0851aa93924aec0908e1f494
Ryan Walters <RyanWaltersDev@gmail.com>
2021-08-23 15:31:04 -0400

    +storing_data branch

---
+++ remember_me.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import json
+
+username = input("What is your name? ")
+
+filename= 'username.json'
+with open(filename, 'w') as f:
+    json.dump(username, f)
+    print(f"We'll remember you when you come back, {username}!")
